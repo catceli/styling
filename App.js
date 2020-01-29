e9beda5490dc1cfbd785b4b37916030836260a84
catceli <catherinedcelio@gmail.com>
2020-01-29 15:30:42 -0800

    book experimenting

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//On 1/29/2020: https://stackoverflow.com/questions/30404067/creating-css-circles-in-react-native
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, View, AppRegistry } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export class FlexboxExamples extends Component {
+  render(){
+    return (
+      <View style={ styles.container }>
+        <View style = { styles.box } />
+        <View style = {[ styles.box, { alignSelf: 'flex-end' } ]} />
+        <View style = { styles.box } />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#e76e63',
+    margin: 10,
   },
 });
+
+export default FlexboxExamples;
+AppRegistry.registerComponent('FlexboxExamples', () => FlexboxExamples);
