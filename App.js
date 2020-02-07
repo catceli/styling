33e6e84a1841ae7668fc8958f899ba43c7b3271f
catceli <catherinedcelio@gmail.com>
2020-02-07 13:32:52 -0800

    added bottom section and title

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 //On 1/29/2020: https://stackoverflow.com/questions/30404067/creating-css-circles-in-react-native
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, View, AppRegistry } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, AppRegistry, Button } from 'react-native';
 
 export class FlexboxExamples extends Component {
+  constructor(props){
+    super(props);
+    this._pressedButton = this._pressedButton.bind(this);
+  }
+
+  _pressedButton(){
+    console.log('hi')
+  }
+
   render(){
     return (
       <View style={[ styles.container ]}>
         <View style = {[ styles.box, { flex: 1 } ]}>
           <View style = {[ styles.circle ]} />
+          <Text style = {[ styles.text ]}>Name Here</Text>
         </View>
-        <View style = {[ styles.box, { backgroundColor: '#8B4513', flex: 2 } ]}>
+        <View style = {[ styles.box, { backgroundColor: '#8B4513', flex: 1.4 } ]}>
+        </View>
+        <View style = {[ styles.box, { backgroundColor: '#302bcc', flex: 0.6 } ]}>
+          <Button
+            style = {[ styles.button, { alignSelf: 'flex-start' } ]}
+            title = 'Back'
+            onPress = {() => this._pressedButton}
+          />
         </View>
       </View>
     );
@@ -26,16 +43,28 @@ const styles = StyleSheet.create({
   box: {
     alignItems: 'center',
     height: 50,
-    backgroundColor: '#e76e63'
+    backgroundColor: '#e76e63',
   },
   circle: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
+    width: 180,
+    height: 180,
+    borderRadius: 90,
     justifyContent: 'center',
     backgroundColor: '#a829c2',
-    margin: 50
+    marginTop: 60,
+    marginBottom: 20,
   },
+  text: {
+    color: '#100812',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 30,
+  },
+  button: {
+    color: '#2bcc91',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 export default FlexboxExamples;
