05d79bb16c1c9d7423a865779e4ed0aab48124a3
catceli <catherinedcelio@gmail.com>
2020-03-08 22:25:57 -0700

    added Jen's notes about indentation and citation location

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-//On 1/29/2020: https://stackoverflow.com/questions/30404067/creating-css-circles-in-react-native
 import React, { Component } from 'react';
 import { Dimensions, StyleSheet, Text, View, AppRegistry, Button, Alert, TouchableHighlight, Image } from 'react-native';
 
@@ -9,22 +8,22 @@ export class FlexboxExamples extends Component {
       <View style={[ styles.container ]}>
         <View style = {[ styles.box, { flex: 1 } ]}>
           <View style = {[ styles.circle ]}>
-          <Image
-            source={require('./sky.png')}
-            style={{ flex: 1, width: null, height: null, borderRadius: 90 }}
-            resizeMode='cover'
-          />
+            <Image
+              source={require('./sky.png')}
+              style={{ flex: 1, width: null, height: null, borderRadius: 90 }}
+              resizeMode='cover'
+            />
           </View>
           <Text style = {[ styles.text ]}>Name Here</Text>
         </View>
         <View style = {[ styles.box, { flex: 1.4 } ]}>
         </View>
         <View style = {[ styles.box, { flex: 0.6, flexDirection: 'row' } ]}>
-        <TouchableHighlight
-          style = {[ styles.button, { flex: 1 } ]}
-          onPress = {() => Alert.alert('hi')}>
-          <Text style = {[ styles.buttonText ]}>Back</Text>
-        </TouchableHighlight>
+          <TouchableHighlight
+            style = {[ styles.button, { flex: 1 } ]}
+            onPress = {() => Alert.alert('hi')}>
+            <Text style = {[ styles.buttonText ]}>Back</Text>
+          </TouchableHighlight>
           <TouchableHighlight
             style = {[ styles.button, { flex: 1 } ]}
             onPress = {() => Alert.alert('hi')}>
@@ -47,6 +46,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: 50,
   },
+  ////On 1/29/2020: https://stackoverflow.com/questions/30404067/creating-css-circles-in-react-native
   circle: {
     width: 180,
     height: 180,
