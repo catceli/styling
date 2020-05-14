66b24e13391240819ee4fb609fc56bcf972487e4
catceli <catherinedcelio@gmail.com>
2020-05-13 19:50:59 -0700

    added basic drawer navigator

--- App.js
+++ App.js
@@ -1,84 +1,52 @@
 import React, { Component } from 'react';
 import { Dimensions, StyleSheet, Text, View, AppRegistry, Button, Alert, TouchableHighlight, Image } from 'react-native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-export class FlexboxExamples extends Component {
+function Home({ navigation }){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  );
+}
 
+function SecondPage({ navigation }){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>SecondPage</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent(props){
+  return(
+    <DrawerContentScrollView{...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Close Drawer"
+        onPress={()=> props.navigation.closeDrawer()}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default class App extends React.Component {
   render(){
     return (
-      <View style={[ styles.container ]}>
-        <View style = {[ styles.box, { flex: 1 } ]}>
-          <View style = {[ styles.circle ]}>
-            <Image
-              source={require('./sky.png')}
-              style={{ flex: 1, width: null, height: null, borderRadius: 90 }}
-              resizeMode='cover'
-            />
-          </View>
-          <Text style = {[ styles.text ]}>Name Here</Text>
-        </View>
-        <View style = {[ styles.box, { flex: 1.4 } ]}>
-        </View>
-        <View style = {[ styles.box, { flex: 0.6, flexDirection: 'row' } ]}>
-          <TouchableHighlight
-            style = {[ styles.button, { flex: 1 } ]}
-            onPress = {() => Alert.alert('hi')}>
-            <Text style = {[ styles.buttonText ]}>Back</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style = {[ styles.button, { flex: 1 } ]}
-            onPress = {() => Alert.alert('hi')}>
-            <Text style = {[ styles.buttonText ]}>Next</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
+      <NavigationContainer>
+        <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="SecondPage" component={SecondPage} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-  },
-  box: {
-    alignItems: 'center',
-    height: 50,
-  },
-<<<<<<< HEAD
-  ////On 1/29/2020: https://stackoverflow.com/questions/30404067/creating-css-circles-in-react-native
-=======
->>>>>>> 6e42eac8a9cb0261003753c5d96ac3d9b3a2b769
-  circle: {
-    width: 180,
-    height: 180,
-    borderRadius: 90,
-    justifyContent: 'center',
-    marginTop: 60,
-    marginBottom: 20,
-    backgroundColor: 'red'
-  },
-  text: {
-    color: '#100812',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 30,
-  },
-  button: {
-    alignContent: 'space-around',
-    margin: 30,
-  },
-  buttonText: {
-    backgroundColor: '#4169E1',
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    overflow: 'hidden',
-    padding: 8,
-    textAlign:'center'
-  },
-});
-
-export default FlexboxExamples;
-AppRegistry.registerComponent('FlexboxExamples', () => FlexboxExamples);
