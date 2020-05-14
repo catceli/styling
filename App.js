3c4c1ef2aff272ee6acc04595d4febb742b74244
catceli <catherinedcelio@gmail.com>
2020-05-13 22:23:37 -0700

    switching w/o merging to stack

--- App.js
+++ App.js
@@ -1,81 +1,41 @@
 import React, { Component } from 'react';
 import { Dimensions, StyleSheet, Text, View, AppRegistry, Button, Alert, TouchableHighlight, Image } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export class FlexboxExamples extends Component {
+function Home({ navigation }){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button
+        title="Go to Second Page"
+        onPress={() => navigation.navigate('SecondPage')}
+      />
+    </View>
+  );
+}
 
+function SecondPage({ navigation }){
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button
+        title="Go back"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
   render(){
     return (
-      <View style={[ styles.container ]}>
-        <View style = {[ styles.box, { flex: 1 } ]}>
-          <View style = {[ styles.circle ]}>
-            <Image
-              source={require('./sky.png')}
-              style={{ flex: 1, width: null, height: null, borderRadius: 90 }}
-              resizeMode='cover'
-            />
-          </View>
-          <Text style = {[ styles.text ]}>Name Here</Text>
-        </View>
-        <View style = {[ styles.box, { flex: 1.4 } ]}>
-        </View>
-        <View style = {[ styles.box, { flex: 0.6, flexDirection: 'row' } ]}>
-          <TouchableHighlight
-            style = {[ styles.button, { flex: 1 } ]}
-            onPress = {() => Alert.alert('hi')}>
-            <Text style = {[ styles.buttonText ]}>Back</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            style = {[ styles.button, { flex: 1 } ]}
-            onPress = {() => Alert.alert('hi')}>
-            <Text style = {[ styles.buttonText ]}>Next</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SecondPage" component={SecondPage} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-  },
-  box: {
-    alignItems: 'center',
-    height: 50,
-  },
-  ////On 1/29/2020: https://stackoverflow.com/questions/30404067/creating-css-circles-in-react-native
-  circle: {
-    width: 180,
-    height: 180,
-    borderRadius: 90,
-    justifyContent: 'center',
-    marginTop: 60,
-    marginBottom: 20,
-    backgroundColor: 'red'
-  },
-  text: {
-    color: '#100812',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 30,
-  },
-  button: {
-    alignContent: 'space-around',
-    margin: 30,
-  },
-  buttonText: {
-    backgroundColor: '#4169E1',
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    overflow: 'hidden',
-    padding: 8,
-    textAlign:'center'
-  },
-});
-
-export default FlexboxExamples;
-AppRegistry.registerComponent('FlexboxExamples', () => FlexboxExamples);
